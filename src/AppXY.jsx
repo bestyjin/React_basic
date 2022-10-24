5920c152e3eead79a1c15350fb141eb551db9f6d
bestyjin <bestyjin0604@gmail.com>
2022-10-24 10:51:53 +0900

    Make Sate to Object

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,18 +2,16 @@ import React, { useState } from "react";
 import "./AppXY.css";
 
 function AppXY(props) {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   const pointer = document.getElementsByClassName("pointer");
   const onPointerMove = (e) => {
-    setX(e.clientX);
-    setY(e.clientY);
+    setPosition({ x: e.clientX, y: e.clientY });
   };
   return (
     <div className="container" onPointerMove={onPointerMove}>
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px,${y}px)` }}
+        style={{ transform: `translate(${position.x}px,${position.y}px)` }}
       ></div>
     </div>
   );
